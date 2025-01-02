a9019e9631a815916c9315326a960f5a163465ba
Sri Charan Bodduna <68746674+sricharan245@users.noreply.github.com>
2025-01-02 15:47:43 -0600

    added new urls

---
+++ config.py
@@ -0,0 +1,25 @@
+def get_urls():
+    company_urls = [
+        'https://walmart.wd5.myworkdayjobs.com/en-US/WalmartExternal?clientRequestID=5af893c0f55c4f7d9c4ff14336a46c1b&jobFamilyGroup=e83ebdbd2a0a01af0185848948e94dc6&locationCountry=bc33aa3152ec42d4995f4791a106ed09',
+        'https://cvshealth.wd1.myworkdayjobs.com/en-US/CVS_Health_Careers?jobFamilyGroup=e65dbadf6a50100168ed7f2a693c0001',
+        'https://nvidia.wd5.myworkdayjobs.com/en-US/NVIDIAExternalCareerSite/job/Senior-Backend-Engineer---Data-Mining_JR1982374?locationHierarchy1=2fcb99c455831013ea52fb338f2932d8',
+        'https://citi.wd5.myworkdayjobs.com/en-US/2/jobs/details/Application-Support-Engineer---Hybrid-_24805666?q=data%20&Country_and_Jurisdiction=bc33aa3152ec42d4995f4791a106ed09&timeType=433d62ef252a012ff4abc7206a0f8000&jobFamilyGroup=e32326e1708d01575bddff0c120102c1&jobFamilyGroup=538c239234271000c428fd3827220000&jobFamilyGroup=e32326e1708d015f865ff60c1201dcc0',
+        'https://keybank.wd5.myworkdayjobs.com/en-US/External_Career_Site/details/XMLNAME-2025-Key-s-Technology--Operations---Services--Technology-Track--Rotational-Analyst-Program---Cleveland_R-26611?q=python',
+        'https://keybank.wd5.myworkdayjobs.com/en-US/External_Career_Site/details/Commercial-Workout-Analyst_R-29449?q=python',
+        'https://sec.wd3.myworkdayjobs.com/en-US/Samsung_Careers/jobs?jobFamilyGroup=189767dd6c9201e189e3eaa6db299dc7&Location_Country=bc33aa3152ec42d4995f4791a106ed09',
+        'https://redhat.wd5.myworkdayjobs.com/en-US/jobs/details/Associate-Software-Engineer--AI-Based-IDE-Plugins-_R-043880-1?e=3afab13eadf301a2eaafadcc15425800&a=bc33aa3152ec42d4995f4791a106ed09&d=c18026e77576010f6ef6126f4e43ec4a&d=c18026e7757601cf6eb0136f4e43f04a',
+        'https://sonyglobal.wd1.myworkdayjobs.com/en-US/SonyGlobalCareers/details/Analyst--Secure-Sony_JR-116643?locationCountry=bc33aa3152ec42d4995f4791a106ed09&jobFamilyGroup=7306bd11847f108d56a689b7002554ab&jobFamilyGroup=7306bd11847f108d56a585fb30065499',
+        'https://santander.wd3.myworkdayjobs.com/en-US/SantanderCareers/details/Sr-Associate--Business-Intelligence---Reporting_Req1393658?jobFamilyGroup=135a3ebce38101db91d599b919013150&jobFamilyGroup=ab9adf92110e011571e710ab1a01034a&jobFamilyGroup=047662461dfb01bf0a10a10a1a01d241&jobFamilyGroup=135a3ebce38101c45b3e18b919012750&source=Santander_com&locationCountry=bc33aa3152ec42d4995f4791a106ed09',
+        'https://cat.wd5.myworkdayjobs.com/en-US/CaterpillarCareers?timeType=5367d85d9d52017a289c2294d7135900&jobFamily=e1ea3238dd28100047a9d21828480000&locationCountry=bc33aa3152ec42d4995f4791a106ed09',
+        'https://intel.wd1.myworkdayjobs.com/en-US/External/details/Supply-Planning-Analyst_JR0267523?jobFamilyGroup=a55ea4dd831d1000c6fce5a0c4d30000&locations=1e4a4eb3adf101cc4e292078bf8199d0&locations=1e4a4eb3adf1016541777876bf8111cf&locations=1e4a4eb3adf1011246675c76bf81f8ce&locations=1e4a4eb3adf101b8aec18a77bf810dd0&locations=1e4a4eb3adf1018c4bf78f77bf8112d0&locations=1e4a4eb3adf10129d05fe377bf815dd0&locations=1e4a4eb3adf10118b1dfe877bf8162d0',
+        'https://homedepot.wd5.myworkdayjobs.com/en-US/CareerDepot/?q=data%20scientist&locationCountry=bc33aa3152ec42d4995f4791a106ed09&workerSubType=3e82bdec28fd014fca89c83b1811b77b',
+        'https://discover.wd5.myworkdayjobs.com/Discover?locations=dd81500376e3100d308a693ac9334f50&locations=dd81500376e3100d308a6dcac9034f5a&locations=dd81500376e3100d308a725565eb4f64&locations=dd81500376e3100d308a95535c334faf&locations=96bf16fa269701eb4279010ddf93b1e3&locations=ae914e0d596801c955d0a23a7a0bb72b&locations=dd81500376e3100d308a8e6d79cb4fa0&locations=dd81500376e3100d308a99d97cab4fb9&jobFamilyGroup=a79d99b763fa01bb186c073e47287154',
+        'https://gartner.wd5.myworkdayjobs.com/EXT?locationCountry=bc33aa3152ec42d4995f4791a106ed09&jobFamilyGroup=5945a98e5950100151b4bdc571a20000',
+        'https://amazon.jobs/en/search?offset=0&result_limit=10&sort=recent&category%5B%5D=machine-learning-science&category%5B%5D=business-intelligence&category%5B%5D=data-science&country%5B%5D=USA&distanceType=Mi&radius=24km&is_manager%5B%5D=0&latitude=&longitude=&loc_group_id=&loc_query=&base_query=&city=&country=&region=&county=&query_options=&',
+        'https://capitalone.wd12.myworkdayjobs.com/en-US/Capital_One/details/Senior-Data-Analyst_R205089-1?jobFamilyGroup=a12c70bf789e105802e9caf800542991'
+
+    ]
+
+
+    return company_urls
+
--- workday.py
+++ workday.py
@@ -10,6 +10,8 @@ import uuid
 from datetime import datetime
 import pytz
 
+from config import get_urls
+
 # Load or initialize job_ids_dict from file
 try:
         with open('job_ids_dict.pkl', 'rb') as f:
@@ -22,23 +24,7 @@ driver = webdriver.Chrome()
 
 wait = WebDriverWait(driver, 10)
 
-company_urls = [
-    'https://walmart.wd5.myworkdayjobs.com/en-US/WalmartExternal?clientRequestID=5af893c0f55c4f7d9c4ff14336a46c1b&jobFamilyGroup=e83ebdbd2a0a01af0185848948e94dc6&locationCountry=bc33aa3152ec42d4995f4791a106ed09',
-    'https://cvshealth.wd1.myworkdayjobs.com/en-US/CVS_Health_Careers?jobFamilyGroup=e65dbadf6a50100168ed7f2a693c0001',
-    'https://nvidia.wd5.myworkdayjobs.com/en-US/NVIDIAExternalCareerSite/job/Senior-Backend-Engineer---Data-Mining_JR1982374?locationHierarchy1=2fcb99c455831013ea52fb338f2932d8',
-    'https://citi.wd5.myworkdayjobs.com/en-US/2/jobs/details/Application-Support-Engineer---Hybrid-_24805666?q=data%20&Country_and_Jurisdiction=bc33aa3152ec42d4995f4791a106ed09&timeType=433d62ef252a012ff4abc7206a0f8000&jobFamilyGroup=e32326e1708d01575bddff0c120102c1&jobFamilyGroup=538c239234271000c428fd3827220000&jobFamilyGroup=e32326e1708d015f865ff60c1201dcc0',
-    'https://keybank.wd5.myworkdayjobs.com/en-US/External_Career_Site/details/XMLNAME-2025-Key-s-Technology--Operations---Services--Technology-Track--Rotational-Analyst-Program---Cleveland_R-26611?q=python',
-    'https://keybank.wd5.myworkdayjobs.com/en-US/External_Career_Site/details/Commercial-Workout-Analyst_R-29449?q=python',
-    'https://sec.wd3.myworkdayjobs.com/en-US/Samsung_Careers/jobs?jobFamilyGroup=189767dd6c9201e189e3eaa6db299dc7&Location_Country=bc33aa3152ec42d4995f4791a106ed09',
-    'https://redhat.wd5.myworkdayjobs.com/en-US/jobs/details/Associate-Software-Engineer--AI-Based-IDE-Plugins-_R-043880-1?e=3afab13eadf301a2eaafadcc15425800&a=bc33aa3152ec42d4995f4791a106ed09&d=c18026e77576010f6ef6126f4e43ec4a&d=c18026e7757601cf6eb0136f4e43f04a',
-    'https://sonyglobal.wd1.myworkdayjobs.com/en-US/SonyGlobalCareers/details/Analyst--Secure-Sony_JR-116643?locationCountry=bc33aa3152ec42d4995f4791a106ed09&jobFamilyGroup=7306bd11847f108d56a689b7002554ab&jobFamilyGroup=7306bd11847f108d56a585fb30065499',
-    'https://santander.wd3.myworkdayjobs.com/en-US/SantanderCareers/details/Sr-Associate--Business-Intelligence---Reporting_Req1393658?jobFamilyGroup=135a3ebce38101db91d599b919013150&jobFamilyGroup=ab9adf92110e011571e710ab1a01034a&jobFamilyGroup=047662461dfb01bf0a10a10a1a01d241&jobFamilyGroup=135a3ebce38101c45b3e18b919012750&source=Santander_com&locationCountry=bc33aa3152ec42d4995f4791a106ed09',
-    'https://cat.wd5.myworkdayjobs.com/en-US/CaterpillarCareers?timeType=5367d85d9d52017a289c2294d7135900&jobFamily=e1ea3238dd28100047a9d21828480000&locationCountry=bc33aa3152ec42d4995f4791a106ed09',
-    'https://intel.wd1.myworkdayjobs.com/en-US/External/details/Supply-Planning-Analyst_JR0267523?jobFamilyGroup=a55ea4dd831d1000c6fce5a0c4d30000&locations=1e4a4eb3adf101cc4e292078bf8199d0&locations=1e4a4eb3adf1016541777876bf8111cf&locations=1e4a4eb3adf1011246675c76bf81f8ce&locations=1e4a4eb3adf101b8aec18a77bf810dd0&locations=1e4a4eb3adf1018c4bf78f77bf8112d0&locations=1e4a4eb3adf10129d05fe377bf815dd0&locations=1e4a4eb3adf10118b1dfe877bf8162d0',
-    'https://homedepot.wd5.myworkdayjobs.com/en-US/CareerDepot/?q=data%20scientist&locationCountry=bc33aa3152ec42d4995f4791a106ed09&workerSubType=3e82bdec28fd014fca89c83b1811b77b',
-    'https://amazon.jobs/en/search?offset=0&result_limit=10&sort=recent&category%5B%5D=machine-learning-science&category%5B%5D=business-intelligence&category%5B%5D=data-science&country%5B%5D=USA&distanceType=Mi&radius=24km&is_manager%5B%5D=0&latitude=&longitude=&loc_group_id=&loc_query=&base_query=&city=&country=&region=&county=&query_options=&'
-
-]  # Add your company URLs here
+company_urls = get_urls()
 
 for company_url in company_urls:
     if company_url not in job_ids_dict:
@@ -131,7 +117,7 @@ while True:
                     
                     next_button.click()
             except Exception as e:
-                print(f"An error occurred while processing {company_url}: {str(e)}")
+                print(f"An error occurred while processing {company_url[:15]}: {str(e)}")
                 continue
     
         print(f"jobs at {company_url.split('//')[1].split('.wd')[0]} \n", len(job_ids_dict[company_urls[0]]))
@@ -160,4 +146,4 @@ while True:
     print(f"At {current_time} total {len(jobs)} jobs added to csv...")
 
     # Wait for a certain period before running again
-    time.sleep(600)  # Run every hour
+    time.sleep(900)  # Run every 15min
